4c721c8ae499435a0d887b2ac903322518e3a9ec
Diego Di Palma <diegodipalma@outlook.com>
2018-01-30 21:25:38 +0100

    Added new library; written some lines

--- Compito_Xamarin_DiPalma/Login.cs
+++ Compito_Xamarin_DiPalma/Login.cs
@@ -1,19 +1,44 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
-
+using SQLite;
+using System;
 namespace Compito_Xamarin_DiPalma
 {
     [Activity(Label = "APP", MainLauncher = true)]
     public class Login : Activity
     {
+        Button btnLogin, btnRecover;
+        TextView txtMail, txtPass;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.Main);
+            SetContentView(Resource.Layout.Login);
+
+            #region data binding
+            btnLogin = FindViewById<Button>(Resource.Id._login);
+            btnRecover = FindViewById<Button>(Resource.Id._recoverPass);
+            txtMail = FindViewById<TextView>(Resource.Id._userMail);
+            txtPass = FindViewById<TextView>(Resource.Id._userPass);
+            #endregion
+
+            btnLogin.Click += BtnLogin_Click;
+            btnRecover.Click += BtnRecover_Click;
+        }
+
+        private void BtnRecover_Click(object sender, EventArgs e)
+        {
+            var connString=new SQLiteConnectionString()
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
+
 }
 
--- Compito_Xamarin_DiPalma/Resources/Resource.Designer.cs
+++ Compito_Xamarin_DiPalma/Resources/Resource.Designer.cs
@@ -57,11 +57,48 @@ namespace Compito_Xamarin_DiPalma
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050006
+			public const int _login = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int _recoverPass = 2131034119;
+			
+			// aapt resource value: 0x7f050003
+			public const int _userMail = 2131034115;
+			
+			// aapt resource value: 0x7f050004
+			public const int _userPass = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int linearLayout1 = 2131034114;
+			
+			// aapt resource value: 0x7f050005
+			public const int linearLayout2 = 2131034117;
+			
+			// aapt resource value: 0x7f050000
+			public const int linearLayout3 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Login = 2130903040;
 			
 			static Layout()
 			{
