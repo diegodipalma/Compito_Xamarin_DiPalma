efdcfcee777c5e7e37d08597ef9cae6915f363f6
Diego Di Palma <diegodipalma@outlook.com>
2018-01-31 21:24:40 +0100

    Edited some lines of code

--- Compito_Xamarin_DiPalma/Login.cs
+++ Compito_Xamarin_DiPalma/Login.cs
@@ -5,6 +5,7 @@ using System;
 using System.IO;
 using System.Text.RegularExpressions;
 using Mono.Data.Sqlite;
+using Android.Content;
 
 namespace Compito_Xamarin_DiPalma
 {
@@ -14,13 +15,14 @@ namespace Compito_Xamarin_DiPalma
         Button btnLogin, btnRecover;
         TextView txtMail, txtPass;
 
-
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Login);
+
+
 
             #region data binding e variabili
             btnLogin = FindViewById<Button>(Resource.Id._login);
@@ -33,9 +35,32 @@ namespace Compito_Xamarin_DiPalma
             btnRecover.Click += BtnRecover_Click;
         }
 
+        public void CopyDatabase(string dataBaseName)
+        {
+            var dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), dataBaseName);
+
+            if (!File.Exists(dbPath))
+            {
+                var dbAssetStream = Assets.Open(dataBaseName);
+                var dbFileStream = new FileStream(dbPath, FileMode.OpenOrCreate);
+                var buffer = new byte[1024];
+
+                int b = buffer.Length;
+                int length;
+
+                while ((length = dbAssetStream.Read(buffer, 0, b)) > 0)
+                {
+                    dbFileStream.Write(buffer, 0, length);
+                }
+
+                dbFileStream.Flush();
+                dbFileStream.Close();
+                dbAssetStream.Close();
+            }
+        }
+
         private void BtnLogin_Click(object sender, EventArgs e)
         {
-            var query = "";
             #region check password con regex
             //// check sul campo password se è vuoto o meno
 
@@ -58,61 +83,66 @@ namespace Compito_Xamarin_DiPalma
             }
             else
             {
-                var nomedb = "data.db";
-                var path = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), nomedb);
-                var connStr = @"Data Source=" + path + ";Version=3;";
-                SqliteConnection connessione = new SqliteConnection(connStr);
+                CopyDatabase("data.db");
+                var dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "data.db");
 
-                try
+                using (var conn = new SqliteConnection("Data Source=" + dbPath))
+                using (var query = new SqliteCommand("select count(*) from Users where Username=@user and Password=@pass", conn)
+                )
                 {
-                    connessione.Open();
-                }
-                catch (Exception ex)
-                {
-                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                    alert.SetTitle("Eccezione");
-                    alert.SetMessage(ex.ToString());
-                    alert.Show();
+                    query.Parameters.AddWithValue("@user", txtMail.Text);
+                    query.Parameters.AddWithValue("@pass", txtPass.Text);
+                    conn.Open();
+                    var risultato = query.ExecuteScalar();
+                    if ((int)risultato == 0)
+                    {
+                        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                        alert.SetTitle("Attenzione");
+                        alert.SetMessage("Non è stato trovato nessun utente con questa combinazione di credenziali!");
+                        alert.Show();
+                        txtMail.Text = "";
+                        txtPass.Text = "";
+                    }
+                    else
+                    {
+                        Intent intent = new Intent(this, typeof(Messaggi));
+                        Bundle b = new Bundle();
+                        b.PutString("user", txtMail.Text);
+                        intent.PutExtras(b);
+                        StartActivity(intent);
+                    }
                 }
 
-                query = "select count(*) from users where username=@user and password=@pass";
-                SqliteCommand queryLogin = new SqliteCommand(query, connessione);
-                queryLogin.Parameters.AddWithValue("@user", txtMail.Text);
-                queryLogin.Parameters.AddWithValue("@pass", CodificaPassword(txtPass.Text, true));
-                var risultato = (Int32)queryLogin.ExecuteScalar();
 
-                if (risultato == 0)
-                {
-                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
-                    alert.SetTitle("Attenzione");
-                    alert.SetMessage("Non è stato trovato nessun utente con questa combinazione di credenziali!");
-                    alert.Show();
-                    txtMail.Text = "";
-                    txtPass.Text = "";
-                }
-                else
+
+
+
+
 
             }
         }
 
         private void BtnRecover_Click(object sender, EventArgs e)
         {
-
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.SetTitle("Funzione non implementata");
+            alert.SetMessage("Ti chiedo scusa ma questa funzione non è ancora implementata! Resta in attesa di una seconda versione.");
+            alert.Show();
         }
 
-        public static string CodificaPassword(string password, bool isBase64)
-        {
-            if (isBase64)
-            {
-                var base64EncodedBytes = Convert.FromBase64String(password);
-                return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
-            }
-            else
-            {
-                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(password);
-                return Convert.ToBase64String(plainTextBytes);
-            }
-        }
+        //public static string CodificaPassword(string password, bool isBase64)
+        //{
+        //    if (isBase64)
+        //    {
+        //        var base64EncodedBytes = Convert.FromBase64String(password);
+        //        return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        //    }
+        //    else
+        //    {
+        //        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(password);
+        //        return Convert.ToBase64String(plainTextBytes);
+        //    }
+        //}
 
     }
 
