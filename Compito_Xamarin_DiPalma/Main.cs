5450bdd40dfa7ed5bd439419b4b403961bf63804
Diego Di Palma <diegodipalma@outlook.com>
2018-01-31 22:13:53 +0100

    1

---
+++ Compito_Xamarin_DiPalma/Main.cs
@@ -0,0 +1,41 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Mono.Data.Sqlite;
+using Android.Content;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Compito_Xamarin_DiPalma
+{
+    [Activity(Label = "BlueChat", MainLauncher = true)]
+    public class Main : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            var docsFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+            var pathToDatabase = Path.Combine(docsFolder, "data.db");
+            if (!File.Exists(pathToDatabase))
+            {
+                SqliteConnection.CreateFile(pathToDatabase);
+
+                using (var conn = new SqliteConnection(pathToDatabase))
+                using (var query = new SqliteCommand(@"CREATE TABLE `Utenti` (`IDUtente` INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, `Username` TEXT NOT NULL UNIQUE, `Password` TEXT NOT NULL, `Soldi` NUMERIC DEFAULT 10.00 CHECK(Soldi > 0));", conn))
+                {
+
+                }
+            }
+
+        }
+
+    }
+}
