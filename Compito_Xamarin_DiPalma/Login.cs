473640eaf3b51f0f5b26604b5a4aad45a395903d
Diego Di Palma <diegodipalma@outlook.com>
2018-02-01 17:56:40 +0100

    1

---
+++ Compito_Xamarin_DiPalma/Login.cs
@@ -0,0 +1,56 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using Mono.Data.Sqlite;
+using Android.Content;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+
+namespace Compito_Xamarin_DiPalma
+{
+    [Activity(Label = "BlueChat", MainLauncher = true)]
+    public class Log : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            var user = FindViewById<TextView>(Resource.Id.txtUser);
+            var pswd = FindViewById<TextView>(Resource.Id.txtPswd);
+            var btnlogin = FindViewById<Button>(Resource.Id.btnLogin);
+            var btnregistra = FindViewById<Button>(Resource.Id.btnRegistra);
+            var btncontatti = FindViewById<Button>(Resource.Id.btnContatti);
+
+            //creazione metodi evento
+            btncontatti.Click += Btncontatti_Click;
+
+        }
+
+        private void Btncontatti_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(Intent.ActionSend);
+            intent.setType("plain/text");
+            intent.putExtra(Intent.ExtraEmail, new String[] { "diegodipalma@outlook.com" });
+            intent.putExtra(Intent.ExtraSubject, "Contatti");
+            intent.putExtra(Intent.ExtraText, "Ciao,");
+            startActivity(Intent.createChooser(intent, ""));
+        }
+
+        private void copyAssetDataBase(string fromfile, string tofile)
+        {
+            using (var source = Assets.Open(fromfile))
+            using (var dest = OpenFileOutput(tofile, FileCreationMode.Private))
+            {
+                source.CopyTo(dest);
+            }
+        }
+
+    }
+}
--- Compito_Xamarin_DiPalma/Resources/Resource.Designer.cs
+++ Compito_Xamarin_DiPalma/Resources/Resource.Designer.cs
@@ -45,7 +45,10 @@ namespace Compito_Xamarin_DiPalma
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int Icon = 2130837504;
+			public const int bluechat = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int Icon = 2130837505;
 			
 			static Drawable()
 			{
@@ -53,6 +56,52 @@ namespace Compito_Xamarin_DiPalma
 			}
 			
 			private Drawable()
+			{
+			}
+		}
+		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f05000a
+			public const int btnContatti = 2131034122;
+			
+			// aapt resource value: 0x7f050006
+			public const int btnLogin = 2131034118;
+			
+			// aapt resource value: 0x7f050007
+			public const int btnRegistra = 2131034119;
+			
+			// aapt resource value: 0x7f050001
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050002
+			public const int linearLayout1 = 2131034114;
+			
+			// aapt resource value: 0x7f050003
+			public const int linearLayout2 = 2131034115;
+			
+			// aapt resource value: 0x7f050008
+			public const int linearLayout3 = 2131034120;
+			
+			// aapt resource value: 0x7f050009
+			public const int textView1 = 2131034121;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView2 = 2131034112;
+			
+			// aapt resource value: 0x7f050005
+			public const int txtPswd = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int txtUser = 2131034116;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
 			{
 			}
 		}
