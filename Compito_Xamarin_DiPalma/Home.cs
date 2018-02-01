79fe63e2a9f4518832ea1b937c12ed1c2167d42c
Diego Di Palma <diegodipalma@outlook.com>
2018-02-01 21:02:30 +0100

    home

--- Compito_Xamarin_DiPalma/Home.cs
+++ Compito_Xamarin_DiPalma/Home.cs
@@ -9,18 +9,39 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Bluetooth;
 
 namespace Compito_Xamarin_DiPalma
 {
     [Activity(Label = "Home", MainLauncher = true)]
     public class Home : Activity
     {
+
+        BluetoothAdapter btAdapter;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your application here
             SetContentView(Resource.Layout.Home);
+
+            btAdapter = BluetoothAdapter.DefaultAdapter;
+            if (btAdapter != null)
+            {
+                if (!btAdapter.IsEnabled)
+                {
+                    Intent intentperbluetooth = new Intent(BluetoothAdapter.ActionRequestEnable);
+                    StartActivityForResult(intentperbluetooth, 2);
+                }
+                else return;
+            }
+            else
+            {
+                Toast.MakeText(this, "Il Bluetooth non sembra disponibile...", ToastLength.Long).Show();
+                Finish();
+                return;
+            }
         }
     }
 }
